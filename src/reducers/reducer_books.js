7d29e073f4903d1a88d54395a5aa67a840319012
Pnguy55 <pnguy55@gmail.com>
2018-08-16 09:08:44 -0500

    Push to gitHub

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'JavaScript: The Good Parts', pages: '101 pages'},
+    {title: 'Harry Potter', pages: '30 pages'},
+    {title: 'The Dark Tower', pages: '900 pages'},
+    {title: 'The Way of Kings', pages: '945 pages'}
+  ]
+}
